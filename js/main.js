a549685ae985e28eb730581e685cb1f60d0e4a8d
Donskyi <132575708+Donskyi@users.noreply.github.com>
2024-01-25 17:11:57 +0200

    My first homework JS

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+// const quantity = prompt("Введіть розмір вашої зарплати");
+
+const salary = 50000;
+let bonus = 0.15 * salary;
+console.log(bonus);
+let totalIncome = salary + bonus;
+console.log(totalIncome);
+let tax = 0.10 * totalIncome;
+console.log(tax);
+let expenses = 190;
+let remainingSalary = salary + bonus - tax - expenses;
+console.log(remainingSalary);
+
+alert("Залишок від вашої зарплати: " + remainingSalary);
